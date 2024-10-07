373183b432b7743a4377c7a40ec8bf1c6b74bd4f
sheezah01 <144109826+sheezah01@users.noreply.github.com>
2024-10-07 00:29:18 -0700

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,28 @@
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
-  };
+  let m = s.length;
+  let n = p.length;
   
-  module.exports = decodeTheRing;
+  const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(false));
+
+  dp[0][0] = true;
+
+  for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === '*') {
+          dp[0][j] = dp[0][j - 1];
+      }
+  }
+
+  for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+          if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
+              dp[i][j] = dp[i - 1][j - 1];
+          } else if (p[j - 1] === '*') {
+              dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+          }
+      }
+  }
+
+  return dp[m][n];
+};
+
+module.exports = decodeTheRing;
