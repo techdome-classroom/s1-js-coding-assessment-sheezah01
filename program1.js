43077bfb8ae2f3a865a3932dd48d186e761641b5
sheezah01 <144109826+sheezah01@users.noreply.github.com>
2024-10-07 00:25:47 -0700

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,8 +1,29 @@
 const getTotalIsles = function (grid) {
+  const dfs = (grid, i, j) => {
+      if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
+          return;
+      }
 
+      grid[i][j] = 'W';
 
-  // write your code here
+      dfs(grid, i - 1, j); // up
+      dfs(grid, i + 1, j); // down
+      dfs(grid, i, j - 1); // left
+      dfs(grid, i, j + 1); // right
+  };
 
+  let num_islands = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+          if (grid[i][j] === 'L') {
+              num_islands++;
+              dfs(grid, i, j);
+          }
+      }
+  }
+
+  return num_islands;
 };
 
 module.exports = getTotalIsles;
